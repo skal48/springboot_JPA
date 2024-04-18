adc2aebb6ce93f277fdcf4b4a1581fbfdd297e0c
skal48 <skal48@naver.com>
2024-04-19 00:17:04 +0900

    5장게시들 읽기:read

--- firstproject/firstproject/src/main/java/com/example/firstproject/Controller/ArticleController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/Controller/ArticleController.java
@@ -6,8 +6,14 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -33,5 +39,28 @@ public class ArticleController {
     log.info(saved.toString());
     return "article/new";
   }
+
+  @GetMapping("/article/{id}")
+  public String show(@PathVariable Long id, Model model){
+    log.info("id: " + id);
+    //1. id를 조회해서 데이터 가져오기
+    Article articleEntity = articleRepository.findById(id).orElse(null);
+    //2. 모델에 데이터 등록하기
+    model.addAttribute("article", articleEntity);
+    //3. 뷰 페이지 반환하기
+    return "article/show";
+  }
+  @GetMapping("/articles")
+  public String index(Model model){
+    //1. 모든 데이터 가져오기
+    //List<Article> articleEntityList= (List<Article>)articleRepository.findAll() 다운캐스팅
+    //Iterable<Article> articleEntityList = articleRepository.findAll() 업캐스팅(List-> Iterable)
+    ArrayList<Article> articleEntityList = articleRepository.findAll();
+    //2. 모델에 데이터 등록하기
+    model.addAttribute("articleList", articleEntityList);
+    //3. 뷰 페이지 설정하기
+    return "article/index";
+  }
+
 
 }
--- firstproject/firstproject/src/main/java/com/example/firstproject/Controller/MemberController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/Controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @Slf4j
@@ -16,12 +20,12 @@ public class MemberController {
   private MemberRepository memberRepository;
 
   @GetMapping("/members")
-  public String members(){
+  public String signupPage() {
     return "members/new";
   }
 
   @PostMapping("/join")
-  public String memberJoin(MemberForm memberForm){
+  public String Join(MemberForm memberForm) {
     //System.out.println(memberForm.toString());
 
     Member member = memberForm.toEntity();
@@ -30,7 +34,20 @@ public class MemberController {
     Member saved = memberRepository.save(member);
     //System.out.println(saved.toString());
     log.info(saved.toString());
-    return"members/new";
+    return "";
   }
 
+  @GetMapping("/member/{id}")
+  public String show(@PathVariable Long id, Model model) {
+    Member memberEntity = memberRepository.findById(id).orElse(null);
+    model.addAttribute("member", memberEntity);
+    return "member/show";
+  }
+
+  @GetMapping("/members")
+  public String index(Model model){
+    ArrayList<Member> memberArrayList = memberRepository.findAll();
+    model.addAttribute("memberArrayList", memberArrayList);
+    return "member/index";
+  }
 }
