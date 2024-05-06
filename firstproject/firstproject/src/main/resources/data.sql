9e15da769264bf15d0fea5ed95f579e0afddd257
skal48 <skal48@naver.com>
2024-05-06 22:59:56 +0900

    11장 셀프체크

--- firstproject/firstproject/src/main/resources/data.sql
+++ firstproject/firstproject/src/main/resources/data.sql
@@ -5,3 +5,8 @@ INSERT INTO article(title, content) VALUES ('다다다', '3333');
 INSERT INTO member(email, password) VALUES ('test1@naver.com', '1111');
 INSERT INTO member(email, password) VALUES ('test2@naver.com', '2222');
 INSERT INTO member(email, password) VALUES ('test3@naver.com', '3333');
+
+INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES ('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES ('카페 모카', '5500');
+
