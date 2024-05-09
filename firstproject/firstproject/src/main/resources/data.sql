cf0719c9f11d7fbe23b90db24ec14a67dfcf3fd2
skal48 <skal48@naver.com>
2024-05-09 23:52:34 +0900

    13장 테스트 케이스 작성하기

--- firstproject/firstproject/src/main/resources/data.sql
+++ firstproject/firstproject/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(title, content) VALUES ('가가가', '1111');
-INSERT INTO article(title, content) VALUES ('나나나', '2222');
-INSERT INTO article(title, content) VALUES ('다다다', '3333');
+INSERT INTO article(title, content) VALUES ('aaa', '1111');
+INSERT INTO article(title, content) VALUES ('bbb', '2222');
+INSERT INTO article(title, content) VALUES ('ccc', '3333');
 
 INSERT INTO member(email, password) VALUES ('test1@naver.com', '1111');
 INSERT INTO member(email, password) VALUES ('test2@naver.com', '2222');
